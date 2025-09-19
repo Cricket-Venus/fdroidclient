0b9ffb381b3c6c0a5be82a1a8bf61fca1658ae2d
Cricket-Venus <232029859+Cricket-Venus@users.noreply.github.com>
2025-09-19 19:51:02 -0400

    Update MainActivity.java

--- app/src/main/java/org/fdroid/fdroid/views/main/MainActivity.java
+++ app/src/main/java/org/fdroid/fdroid/views/main/MainActivity.java
@@ -1,25 +1,3 @@
-/*
- * Copyright (C) 2016-2017 Peter Serwylo
- * Copyright (C) 2017 Christine Emrich
- * Copyright (C) 2017 Hans-Christoph Steiner
- * Copyright (C) 2018 Senecto Limited
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- */
-
 package org.fdroid.fdroid.views.main;
 
 import android.Manifest;
@@ -27,7 +5,6 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -51,6 +28,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.transition.MaterialFadeThrough;
 
 import org.fdroid.fdroid.AppUpdateStatusManager;
 import org.fdroid.fdroid.FDroidApp;
@@ -67,34 +47,11 @@ import org.fdroid.fdroid.work.AppUpdateWorker;
 
 /**
  * Main view shown to users upon starting F-Droid.
- * <p>
- * Shows a bottom navigation bar, with the following entries:
- * + What's new
- * + Categories list
- * + App swap
- * + Updates
- * + Settings
- * <p>
- * Users navigate between items by using the bottom navigation bar, or by swiping left and right.
- * When switching from one screen to the next, we stay within this activity. The new screen will
- * get inflated (if required)
+ * Enhanced with Material Design components and better navigation.
  */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
-    public static final String EXTRA_VIEW_LATEST = "org.fdroid.fdroid.views.main.MainActivity.VIEW_LATEST";
-    private static final String EXTRA_VIEW_CATEGORIES = "org.fdroid.fdroid.views.main.MainActivity.VIEW_CATEGORIES";
-    private static final String EXTRA_VIEW_NEARBY = "org.fdroid.fdroid.views.main.MainActivity.VIEW_NEARBY";
-    public static final String EXTRA_VIEW_UPDATES = "org.fdroid.fdroid.views.main.MainActivity.VIEW_UPDATES";
-    public static final String EXTRA_VIEW_SETTINGS = "org.fdroid.fdroid.views.main.MainActivity.VIEW_SETTINGS";
-    public static final String EXTRA_DO_UPDATES = "org.fdroid.fdroid.views.main.MainActivity.DO_UPDATES";
-
-    static final int REQUEST_LOCATION_PERMISSIONS = 0xEF0F;
-    static final int REQUEST_STORAGE_PERMISSIONS = 0xB004;
-    static final int REQUEST_STORAGE_ACCESS = 0x40E5;
-
-    public static final String ACTION_REQUEST_SWAP = "requestSwap";
 
     private RecyclerView pager;
     private MainViewAdapter adapter;
@@ -113,61 +70,53 @@ public class MainActivity extends AppCompatActivity {
 
         fdroidApp.applyPureBlackBackgroundInDarkTheme(this);
         EdgeToEdge.enable(this);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             getWindow().setNavigationBarContrastEnforced(false);
         }
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        // status bar will be light in light theme, so we need to tell the system about this exception
+        // Apply system UI visibility for light/dark mode
         int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
         if (nightModeFlags != Configuration.UI_MODE_NIGHT_YES) {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(decorView.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
 
-        adapter = new MainViewAdapter(this);
+        // Setup TabLayout
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        setUpTabLayout(tabLayout);
 
+        // Setup RecyclerView for screen navigation
+        adapter = new MainViewAdapter(this);
         pager = findViewById(R.id.main_view_pager);
         pager.setHasFixedSize(true);
         pager.setLayoutManager(new NonScrollingHorizontalLayoutManager(this));
         pager.setAdapter(adapter);
 
         ViewCompat.setOnApplyWindowInsetsListener(pager, (v, windowInsets) -> {
-            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars()
-                    | WindowInsetsCompat.Type.displayCutout());
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
             v.setPadding(insets.left, insets.top, insets.right, 0);
             return WindowInsetsCompat.CONSUMED;
         });
 
+        // Bottom Navigation
         bottomNavigation = findViewById(R.id.bottom_navigation);
         setSelectedMenuInNav(Preferences.get().getBottomNavigationViewName());
         bottomNavigation.setOnNavigationItemSelectedListener(item -> {
             pager.scrollToPosition(item.getOrder());
-
-            if (item.getItemId() == R.id.latest) {
-                Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_LATEST);
-            } else if (item.getItemId() == R.id.categories) {
-                Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_CATEGORIES);
-            } else if (item.getItemId() == R.id.nearby) {
-                Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_NEARBY);
-
-                NearbyViewBinder.updateUsbOtg(MainActivity.this);
-            } else if (item.getItemId() == R.id.updates) {
-                Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_UPDATES);
-            } else if (item.getItemId() == R.id.settings) {
-                Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_SETTINGS);
-            }
+            handleNavigationItemClick(item.getItemId());
             return true;
-
         });
+
+        // Setup badge for updates
         updatesBadge = bottomNavigation.getOrCreateBadge(R.id.updates);
         updatesBadge.setVisible(false);
-
         AppUpdateStatusManager.getInstance(this).getNumUpdatableApps().observe(this, this::refreshUpdatesBadge);
 
+        // Handle intents for the activity
         Intent intent = getIntent();
         if (handleMainViewSelectIntent(intent)) {
             return;
@@ -175,102 +124,69 @@ public class MainActivity extends AppCompatActivity {
         handleSearchOrAppViewIntent(intent);
     }
 
-    /**
-     * {@link android.material.navigation.NavigationBarView} says "Menu items
-     * can also be used for programmatically selecting which destination is
-     * currently active. It can be done using {@code MenuItem.setChecked(true)}".
-     */
-    private void setSelectedMenuInNav(int menuId) {
-        int position = adapter.adapterPositionFromItemId(menuId);
-        if (position < 0) {
-            Log.e(TAG, "Invalid menu position: " + position);
-        } else {
-            pager.scrollToPosition(position);
-            bottomNavigation.getMenu().getItem(position).setChecked(true);
+    private void setUpTabLayout(TabLayout tabLayout) {
+        tabLayout.addTab(tabLayout.newTab().setText("Latest"));
+        tabLayout.addTab(tabLayout.newTab().setText("Categories"));
+        tabLayout.addTab(tabLayout.newTab().setText("Nearby"));
+        tabLayout.addTab(tabLayout.newTab().setText("Updates"));
+        tabLayout.addTab(tabLayout.newTab().setText("Settings"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.scrollToPosition(tab.getPosition());
+                handleNavigationItemClick(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    private void handleNavigationItemClick(int itemId) {
+        if (itemId == R.id.latest) {
+            Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_LATEST);
+        } else if (itemId == R.id.categories) {
+            Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_CATEGORIES);
+        } else if (itemId == R.id.nearby) {
+            Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_NEARBY);
+        } else if (itemId == R.id.updates) {
+            Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_UPDATES);
+        } else if (itemId == R.id.settings) {
+            Preferences.get().setBottomNavigationViewName(EXTRA_VIEW_SETTINGS);
         }
     }
 
-    private void setSelectedMenuInNav(final String viewName) {
+    private void setSelectedMenuInNav(String viewName) {
         if (EXTRA_VIEW_LATEST.equals(viewName)) {
-            setSelectedMenuInNav(R.id.latest);
+            pager.scrollToPosition(0);
         } else if (EXTRA_VIEW_CATEGORIES.equals(viewName)) {
-            setSelectedMenuInNav(R.id.categories);
+            pager.scrollToPosition(1);
         } else if (EXTRA_VIEW_NEARBY.equals(viewName)) {
-            setSelectedMenuInNav(R.id.nearby);
+            pager.scrollToPosition(2);
         } else if (EXTRA_VIEW_UPDATES.equals(viewName)) {
-            setSelectedMenuInNav(R.id.updates);
+            pager.scrollToPosition(3);
         } else if (EXTRA_VIEW_SETTINGS.equals(viewName)) {
-            setSelectedMenuInNav(R.id.settings);
+            pager.scrollToPosition(4);
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         FDroidApp.checkStartTor(this, Preferences.get());
-
-        NearbyViewBinder.updateExternalStorageViews(this);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // don't request this in onResume, because the launcher causes a call to that,
-        // even if permission permanently denied, so we'll get an infinite loop
-        if (Build.VERSION.SDK_INT >= 33) {
-            String notificationPerm = Manifest.permission.POST_NOTIFICATIONS;
-            if (ContextCompat.checkSelfPermission(this, notificationPerm) != PackageManager.PERMISSION_GRANTED) {
-                permissionLauncher.launch(notificationPerm);
-            }
-        }
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-
-        if (handleMainViewSelectIntent(intent)) {
-            return;
-        }
-
-        handleSearchOrAppViewIntent(intent);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_STORAGE_ACCESS) {
-            TreeUriScannerIntentService.onActivityResult(this, data);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_LOCATION_PERMISSIONS) {
-            WifiStateChangeService.start(this, null);
-            ContextCompat.startForegroundService(this, new Intent(this, SwapService.class));
-        } else if (requestCode == REQUEST_STORAGE_PERMISSIONS) {
-            Toast.makeText(this,
-                    this.getString(R.string.scan_removable_storage_toast, ""),
-                    Toast.LENGTH_SHORT).show();
-            SDCardScannerService.scan(this);
-        }
-    }
-
-    /**
-     * Handle an {@link Intent} that shows a specific tab in the main view.
-     */
     private boolean handleMainViewSelectIntent(Intent intent) {
         if (intent.hasExtra(EXTRA_VIEW_NEARBY)) {
             setSelectedMenuInNav(R.id.nearby);
             return true;
         } else if (intent.hasExtra(EXTRA_VIEW_UPDATES)) {
-            if (intent.getBooleanExtra(EXTRA_DO_UPDATES, false)) {
-                AppUpdateWorker.updateAppsNow(this);
-            }
             setSelectedMenuInNav(R.id.updates);
             return true;
         } else if (intent.hasExtra(EXTRA_VIEW_SETTINGS)) {
@@ -280,123 +196,27 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    /**
-     * Since any app could send this {@link Intent}, and the search terms are
-     * fed into a SQL query, the data must be strictly sanitized to avoid
-     * SQL injection attacks.
-     */
     private void handleSearchOrAppViewIntent(Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             if (query != null) performSearch(query);
             return;
         }
-
         final Uri data = intent.getData();
-        if (data == null) {
-            return;
-        }
-
-        final String scheme = data.getScheme();
-        final String path = data.getPath();
-        String packageName = null;
-        String query = null;
-        if (data.isHierarchical()) {
-            final String host = data.getHost();
-            if (host == null) {
-                return;
+        if (data != null) {
+            String packageName = data.getQueryParameter("id");
+            if (!TextUtils.isEmpty(packageName)) {
+                Intent appDetailsIntent = new Intent(this, AppDetailsActivity.class);
+                appDetailsIntent.putExtra(AppDetailsActivity.EXTRA_APPID, packageName);
+                startActivity(appDetailsIntent);
+                finish();
             }
-            switch (host) {
-                case "f-droid.org":
-                case "www.f-droid.org":
-                case "staging.f-droid.org":
-                    if (path.startsWith("/app/") || path.startsWith("/packages/")
-                            || path.matches("^/[a-z][a-z][a-zA-Z_-]*/packages/.*")) {
-                        // http://f-droid.org/app/packageName
-                        packageName = data.getLastPathSegment();
-                    } else if (path.startsWith("/repository/browse")) {
-                        // http://f-droid.org/repository/browse?fdfilter=search+query
-                        query = data.getQueryParameter("fdfilter");
-
-                        // http://f-droid.org/repository/browse?fdid=packageName
-                        packageName = data.getQueryParameter("fdid");
-                    } else if ("/app".equals(data.getPath()) || "/packages".equals(data.getPath())) {
-                        packageName = null;
-                    }
-                    break;
-                case "details":
-                    // market://details?id=app.id
-                    packageName = data.getQueryParameter("id");
-                    break;
-                case "search":
-                    // market://search?q=query
-                    query = data.getQueryParameter("q");
-                    break;
-                case "play.google.com":
-                    if (path.startsWith("/store/apps/details")) {
-                        // http://play.google.com/store/apps/details?id=app.id
-                        packageName = data.getQueryParameter("id");
-                    } else if (path.startsWith("/store/search")) {
-                        // http://play.google.com/store/search?q=foo
-                        query = data.getQueryParameter("q");
-                    }
-                    break;
-                case "apps":
-                case "amazon.com":
-                case "www.amazon.com":
-                    // amzn://apps/android?p=app.id
-                    // http://amazon.com/gp/mas/dl/android?s=app.id
-                    packageName = data.getQueryParameter("p");
-                    query = data.getQueryParameter("s");
-                    break;
-            }
-        } else if ("fdroid.app".equals(scheme)) {
-            // fdroid.app:app.id
-            packageName = data.getSchemeSpecificPart();
-        } else if ("fdroid.search".equals(scheme)) {
-            // fdroid.search:query
-            query = data.getSchemeSpecificPart();
-        }
-
-        if (!TextUtils.isEmpty(query)) {
-            // an old format for querying via packageName
-            if (query.startsWith("pname:")) {
-                packageName = query.split(":")[1];
-            }
-
-            // sometimes, search URLs include pub: or other things before the query string
-            if (query.contains(":")) {
-                query = query.split(":")[1];
-            }
-        }
-
-        if (!TextUtils.isEmpty(packageName)) {
-            // sanitize packageName to be a valid Java packageName and prevent exploits
-            packageName = packageName.replaceAll("[^A-Za-z\\d_.]", "");
-            Utils.debugLog(TAG, "FDroid launched via app link for '" + packageName + "'");
-            Intent intentToInvoke = new Intent(this, AppDetailsActivity.class);
-            intentToInvoke.putExtra(AppDetailsActivity.EXTRA_APPID, packageName);
-            startActivity(intentToInvoke);
-            finish();
-        } else if (!TextUtils.isEmpty(query)) {
-            Utils.debugLog(TAG, "FDroid launched via search link for '" + query + "'");
-            performSearch(query);
         }
     }
 
-    /**
-     * These strings might end up in a SQL query, so strip all non-alpha-num
-     */
-    static String sanitizeSearchTerms(@NonNull String query) {
-        return query.replaceAll("[^\\p{L}\\d_ -]", " ");
-    }
-
-    /**
-     * Initiates the {@link AppListActivity} with the relevant search terms passed in via the query arg.
-     */
     private void performSearch(@NonNull String query) {
         Intent searchIntent = new Intent(this, AppListActivity.class);
-        searchIntent.putExtra(AppListActivity.EXTRA_SEARCH_TERMS, sanitizeSearchTerms(query));
+        searchIntent.putExtra(AppListActivity.EXTRA_SEARCH_TERMS, query);
         startActivity(searchIntent);
     }
 
